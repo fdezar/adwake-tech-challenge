19c1bc3a58248f3af52ca9c6402566a376d5a829
Gabriel Fernandez <gfernandezareas@gmail.com>
2024-09-23 23:12:01 +0200

    importing data... loading visual feature implemented

--- client/src/app/league-selector/league-selector.component.ts
+++ client/src/app/league-selector/league-selector.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FootballApiService } from '../football-api.service';
 
@@ -8,10 +9,11 @@ import { FootballApiService } from '../football-api.service';
   standalone: true,
   templateUrl: './league-selector.component.html',
   styleUrls: ['./league-selector.component.css'],
-  imports: [FormsModule]
+  imports: [FormsModule, CommonModule]
 })
 export class LeagueSelectorComponent {
   leagueCode: string = '';
+  isLoading: boolean = false;
 
   constructor(private router: Router, private apiService: FootballApiService) {}
 
@@ -22,13 +24,16 @@ export class LeagueSelectorComponent {
   }
 
   importLeague() {
+    this.isLoading = true;
     if (this.leagueCode) {
       this.apiService.importLeague(this.leagueCode).subscribe(
         response => {
           console.log('Import successful:', response);
+          this.isLoading = false;
         },
         error => {
           console.error('Error importing league:', error);
+          this.isLoading = false;
         }
       );
     }
